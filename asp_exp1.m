c47dda915786fa4a6f91ff5540a25a06c628990b
KausthubK\META-K <kausthub.k@live.com>
2019-03-11 14:43:47 +0000

    Create asp_exp1.m
    created stub

---
+++ asp_exp1.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% @author: Kausthub Krishnamurthy
+% URN:     6562233
+% EEEM007 Advanced Signal Processing - Lab Experiments
+% Filename: asp_exp1.m
+% Date started: 11-Mar-2019
+% Date updated: 11-Mar-2019
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% 0. Clear everything
+clear all
+close all
+clc
+
+%% 1. 
