18e66c35fca82f7db747ee2bae14824e86f18fcd
Krishnamurthy <kk00730@otter31.eps.surrey.ac.uk>
2019-03-22 13:59:27 +0000

    exercise 1 corrected. ex2 stub started

---
+++ asp_exp2.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% @author: Kausthub Krishnamurthy
+% URN:     6562233
+% EEEM007 Advanced Signal Processing - Lab Experiments
+% Filename: asp_exp2.m
+% Date started: 18-Mar-2019
+% Date updated: 18-Mar-2019
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% 0. Clear everything
+
+clear all
+close all
+clc
+
+%% 1. 
